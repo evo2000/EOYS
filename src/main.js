31ce676edfaee91092f3485e171ea8a4936f00c5
Jonathan Lam <jlam55555@gmail.com>
2021-05-17 11:15:50 -0400

    added vue-router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import {createRouter, createWebHistory} from 'vue-router';
 
-createApp(App).mount('#app')
+const routes = [
+	{path: '/', component: HelloWorld},
+
+	// add more routes here as necessary
+];
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
