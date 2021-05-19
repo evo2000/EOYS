532d5773fb4be40fab88be0c295bbff25d19ad91
Jonathan Lam <jlam55555@gmail.com>
2021-05-19 18:27:25 -0400

    updated routing baseurl for github pages + 404 page

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import splash from './components/splash.vue';
-import project from './components/project.vue';
-import projectlist from './components/project-list.vue';
+import Splash from './components/splash.vue';
+import Project from './components/project.vue';
+import ProjectList from './components/project-list.vue';
+import PageNotFound from './components/page-not-found.vue';
 import {createRouter, createWebHistory} from 'vue-router';
 
-const routes = [
-	{path: '/', component: splash},
-	{path: '/project-list', component: projectlist},
-	{path: '/project/:projectid', component: project},
+let routes = [
+	{path: '/', component: Splash},
+	{path: '/projects', component: ProjectList},
+	{path: '/projects/:projectid', component: Project},
 
 	// add more routes here as necessary
 
 	// 404 page
-	// { path: '/:pathMatch(.*)*', component: PathNotFound },
+	{ path: '/:pathMatch(.*)*', component: PageNotFound },
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHistory(process.env.NODE_ENV === 'production' ? '/EOYS' : '/'),
 	routes,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+	plugins: [vue()],
+
+	// for serving at evo2000.github.io/EOYS/*
+	base: process.env.NODE_ENV === 'production' ? '/EOYS/' : '/',
 })
