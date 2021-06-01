329c1fcf3cf25b8ebbdfdf29fb6977b3a1aa31dd
evo2000 <57459015+evo2000@users.noreply.github.com>
2021-06-01 16:00:28 -0400

    CNAME added

--- src/components/page-not-found.vue
+++ src/components/page-not-found.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <h1>404: You're lost</h1>
         <h3>... or we messed up.</h3>
-        <p>If you think it's the latter, please contact one of the webmasters (found on the <router-link to="/about">About page</router-link>), and give them the path this link:</p>
+        <p>If you think it's the latter, please contact one of the webmasters (found on the <router-link to="/about">About page</router-link>), and give them this path:</p>
         <p><code>{{$route.fullPath}}</code></p>
         <router-link to="/">Return to the homepage.</router-link>
     </div>
